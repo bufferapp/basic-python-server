17a7e11fb30e6fae87f4998f90da33881868814b
Adnan Issadeen <eddyrox1@hotmail.com>
2020-08-18 12:51:35 +0000

    Added some logging

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
   if request.method == 'GET':
+    print('The request was a get')
     print(request.headers)
     print(request.remote_addr)
   if request.method == 'POST':
