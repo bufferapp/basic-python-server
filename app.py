bff42fdecd5a8e135c5a9f54edada296a6263278
Adnan Issadeen <eddyrox1@hotmail.com>
2020-08-18 12:54:05 +0000

    Added more logging

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
+  print(request.headers)
+  print(request.remote_addr)
   if request.method == 'GET':
     print('The request was a get')
     print(request.headers)
@@ -10,4 +12,4 @@ def hello_world():
   if request.method == 'POST':
     print(request.json)
     print(request.form)
-  return {'test': 'worked'}
+  return {'test': 'it worked'}
