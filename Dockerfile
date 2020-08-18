ef8465b6e8e4f6a3f8cee84be206af6cfaa80837
Adnan Issadeen <eddyrox1@hotmail.com>
2020-08-18 12:57:14 +0000

    Unbuffered printing

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ COPY app.py /src/app.py
 WORKDIR /src
 
 ENV FLASK_APP=app.py
+ENV PYTHONUNBUFFERED=nonempty
 EXPOSE 5000
 CMD flask run -h 0.0.0.0
--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 def hello_world():
   print(request.headers)
   print(request.remote_addr)
+  
   if request.method == 'GET':
     print('The request was a get')
     print(request.headers)
